1604f022501f12836286dc90c900cafac13baed7
Kanokpit <thana.drnn@hotmail.com>
2025-08-04 19:24:54 +0700

    fix: more workshop link

--- src/app/ssg/page.tsx
+++ src/app/ssg/page.tsx
@@ -1,19 +1,11 @@
 import { Metadata } from 'next';
+import Link from 'next/link';
 
 export const metadata: Metadata = {
 	title: 'Static Site Generation (SSG)',
 };
 
-async function getData() {
-	// ดึงข้อมูลล่วงหน้าใน build time
-	const res = await fetch('https://jsonplaceholder.typicode.com/posts/1');
-	if (!res.ok) throw new Error('Failed to fetch data');
-	return res.json();
-}
-
 export default async function SSGPage() {
-	const data = await getData();
-
 	return (
 		<div className="p-8">
 			<h1 className="font-semibold">Static Site Generation (SSG)</h1>
@@ -24,9 +16,9 @@ export default async function SSGPage() {
 				ทำให้เว็บไซต์โหลดเร็วและมีความปลอดภัยสูง
 			</h4>
 
-			<div className="my-6">
-				<p>Title: {data.title}</p>
-				<p>Body: {data.body}</p>
+			<div className="flex items-center gap-2 my-6">
+				Workshop เพิ่มเติม:
+				<Link href={`https://github.com/kanokpit-yowaratch/ssg-workshop`}>SSG Workshop</Link>
 			</div>
 		</div>
 	);
